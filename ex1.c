0dddd70a93bcae0e35ddf631e10ee42944752203
Phil <birkal@outlook.com>
2014-06-21 16:49:40 -0700

    Exercise 1: Dust Off That Compiler

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello world.");
+	puts("This needs to more complicated then hello world.");
+	puts("Extra credit saids I need 5 more lines.");
+	puts("Hmm this should work \" right? Somehow using single quotes gave me a Segmentation fault whatever that means.");
+	puts("Turns out a segmentation fault (often shortened to segfault) or access violation is a fault raised by hardware with memory protection, notifying an operating system (OS) about a memory access violation; on x86 computers this is a form of general protection fault.");
+	puts("Hopefully escape sequences \n\t\"work\".");
+
+	return 0;
+}
