68c4eb82e72be866bd1f2341100a77f522f6a404
Phil <birkal@birkal@outlook.com>
2014-06-26 12:35:16 -0700

    Exercise 5: The Structure Of A C Program

---
+++ ex5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* This is a comment. 
+	@params argc is an int for the argument count
+			*argv is an array of char * strings
+*/
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	// this is also a comment
+	printf("You are %d miles away.\n", distance);
+
+	return 0;
+}
